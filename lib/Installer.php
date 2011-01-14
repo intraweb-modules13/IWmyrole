56aeed409f11ca01cbdd2cc2028cbbfd82d7a1e3
aperezm <aperezm@e968f4be-1634-4921-b96b-e3dd0a78059a>
2011-01-14 10:50:34 +0000

    Refactoring modules to 1.3

--- lib/Installer.php
+++ lib/Installer.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * PostNuke Application Framework
  *
@@ -9,101 +10,98 @@
  * @package PostNuke_Value_Addons
  * @subpackage Noteboard
  */
+class IWmyrole_Installer extends Zikula_Installer {
+    /**
+     * Initialise the IWmyrole module creating module vars
+     * @author Albert Pérez Monfort (aperezm@xtec.cat)
+     * @author Josep Ferràndiz Farré (jferran6@xtec.cat)
+     * @return bool true if successful, false otherwise
+     */
+    public function install() {
+        $dom = ZLanguage::getModuleDomain('IWmyrole');
+        // Checks if module IWmain is installed. If not returns error
+        $modid = ModUtil::getIdFromName('IWmain');
+        $modinfo = ModUtil::getInfo($modid);
 
-/**
- * Initialise the IWmyrole module creating module vars
- * @author Albert Pérez Monfort (aperezm@xtec.cat)
- * @author Josep Ferràndiz Farré (jferran6@xtec.cat)
- * @return bool true if successful, false otherwise
- */
-function IWmyrole_init()
-{
-	$dom = ZLanguage::getModuleDomain('IWmyrole');
-	// Checks if module IWmain is installed. If not returns error
-	$modid = ModUtil::getIdFromName('IWmain');
-	$modinfo = ModUtil::getInfo($modid);
-	
-	if($modinfo['state'] != 3){
-		return LogUtil::registerError (__('Module IWmain is required. You have to install the IWmain module previously to install it.',$dom));
-	}
-	
-	// Check if the version needed is correct
-	$versionNeeded = '2.0';
-	if(!ModUtil::func('IWmain', 'admin', 'checkVersion', array('version' => $versionNeeded))){
-		return false;
-	}
-	
-	// Name of the initial group
-	$name = "changingRole";
+        if ($modinfo['state'] != 3) {
+            return LogUtil::registerError(__('Module IWmain is required. You have to install the IWmain module previously to install it.', $dom));
+        }
 
-	// The API function is called.
-	$check = ModUtil::apiFunc('Groups', 'admin', 'getgidbyname', array('name' => $name));
+        // Check if the version needed is correct
+        $versionNeeded = '2.0';
+        if (!ModUtil::func('IWmain', 'admin', 'checkVersion', array('version' => $versionNeeded))) {
+            return false;
+        }
 
-	if ($check != false) {
-		// Group already exists
-		// LogUtil::registerError (_GROUPS_ALREADYEXISTS);
-		$gid = $check;
-	} else {
-		// Falta mirar si existeix
-		$gid = ModUtil::apiFunc('Groups', 'admin', 'create', array('name' => $name,
-								                                  'gtype' => 0,
-								                                  'state' => 0,
-								                                  'nbumax'  => 0,
-								                                  'description' => __('Initial group of users that can change the role',$dom)));
-		// Success
-		($gid)?LogUtil::registerStatus (pnML('_CREATEITEMSUCCEDED', array('i' => $name))):"";
+        // Name of the initial group
+        $name = "changingRole";
 
-	}
+        // The API function is called.
+        $check = ModUtil::apiFunc('Groups', 'admin', 'getgidbyname', array('name' => $name));
 
-	// The return value of the function is checked here
-	if ($gid != false) {
-		ModUtil::setVar( 'IWmyrole', 'rolegroup', $gid);
-		// Gets the first sequence number of permissions list
-		$pos = DBUtil::selectFieldMax( 'group_perms', 'sequence', 'MIN');
-		// SET MODULE AND BLOCK PERMISSIONS
-		// insert permission myrole:: :: administration in second place
-		ModUtil::apiFunc('permissions','admin', 'create', array('realm' => 0,
-															'id' => $gid,
-															'component' => 'IWmyrole::',
-															'instance' => '::' ,
-															'level' => '800',
-															'insseq' => $pos + 1 ));	
-	}
+        if ($check != false) {
+            // Group already exists
+            // LogUtil::registerError (_GROUPS_ALREADYEXISTS);
+            $gid = $check;
+        } else {
+            // Falta mirar si existeix
+            $gid = ModUtil::apiFunc('Groups', 'admin', 'create', array('name' => $name,
+                        'gtype' => 0,
+                        'state' => 0,
+                        'nbumax' => 0,
+                        'description' => __('Initial group of users that can change the role', $dom)));
+            // Success
+            ($gid) ? LogUtil::registerStatus(pnML('_CREATEITEMSUCCEDED', array('i' => $name))) : "";
+        }
 
-	ModUtil::setVar( 'IWmyrole', 'groupsNotChangeable', '');
+        // The return value of the function is checked here
+        if ($gid != false) {
+            ModUtil::setVar('IWmyrole', 'rolegroup', $gid);
+            // Gets the first sequence number of permissions list
+            $pos = DBUtil::selectFieldMax('group_perms', 'sequence', 'MIN');
+            // SET MODULE AND BLOCK PERMISSIONS
+            // insert permission myrole:: :: administration in second place
+            ModUtil::apiFunc('permissions', 'admin', 'create', array('realm' => 0,
+                        'id' => $gid,
+                        'component' => 'IWmyrole::',
+                        'instance' => '::',
+                        'level' => '800',
+                        'insseq' => $pos + 1));
+        }
 
-	//Initialation successfull
-  	return true;
-}
+        ModUtil::setVar('IWmyrole', 'groupsNotChangeable', '');
 
-/**
- * Delete the iw_noteboard module
- * @author Albert Pérez Monfort (aperezm@xtec.cat)
- * @author Josep Ferràndiz Farré (jferran6@xtec.cat)
- * @return bool true if successful, false otherwise
- */
-function IWmyrole_delete()
-{
-	//Deletion successfull
-	// Esborrar el permís IWmyrole
-    $pntables = DBUtil::getTables();	
-    $column   = $pntables['group_perms_column'];
+        //Initialation successfull
+        return true;
+    }
 
-	$where = "WHERE $column[component] LIKE 'IWmyrole%' AND $column[gid] = ".ModUtil::getVar('IWmyrole', 'rolegroup');
-	$result = DBUtil::DeleteWhere('group_perms', $where);
+    /**
+     * Delete the iw_noteboard module
+     * @author Albert Pérez Monfort (aperezm@xtec.cat)
+     * @author Josep Ferràndiz Farré (jferran6@xtec.cat)
+     * @return bool true if successful, false otherwise
+     */
+    public function uninstall() {
+        //Deletion successfull
+        // Esborrar el permís IWmyrole
+        $pntables = DBUtil::getTables();
+        $column = $pntables['group_perms_column'];
 
-	ModUtil::delVar('my_role', 'rolegroup');
-	ModUtil::delVar( 'IWmyrole', 'groupsNotChangeable');
+        $where = "WHERE $column[component] LIKE 'IWmyrole%' AND $column[gid] = " . ModUtil::getVar('IWmyrole', 'rolegroup');
+        $result = DBUtil::DeleteWhere('group_perms', $where);
 
-	return true;
-}
+        ModUtil::delVar('my_role', 'rolegroup');
+        ModUtil::delVar('IWmyrole', 'groupsNotChangeable');
 
-/**
- * Update the iw_noteboard module
- * @author Albert Pérez Monfort (aperezm@xtec.cat)
- * @return bool true if successful, false otherwise
- */
-function IWmyrole_upgrade($oldversion)
-{
-	return true;
+        return true;
+    }
+
+    /**
+     * Update the iw_noteboard module
+     * @author Albert Pérez Monfort (aperezm@xtec.cat)
+     * @return bool true if successful, false otherwise
+     */
+    public function upgrade($oldversion) {
+        return true;
+    }
 }
--- lib/Version.php
+++ lib/Version.php
@@ -5,22 +5,23 @@
  * @author		Albert Pérez Monfort (aperezm@xtec.cat)
  * @return		The version information
  */
-$dom = ZLanguage::getModuleDomain('IWmyrole');
-$modversion['name'] = 'IWmyrole';
-$modversion['version'] = '2.0';
-$modversion['description'] = __('Allow users to change their roles or groups.', $dom);
-$modversion['displayname'] = __('myRole', $dom);
-$modversion['url']=__('IWmyrole', $dom);
-$modversion['credits'] = 'pndocs/credits.txt';
-$modversion['help'] = 'pndocs/help.txt';
-$modversion['changelog'] = 'pndocs/changelog.txt';
-$modversion['license'] = 'pndocs/license.txt';
-$modversion['official'] = 0;
-$modversion['author'] = 'Albert Pérez Monfort & Josep Ferràndiz Farré';
-$modversion['contact'] = 'aperezm@xtec.es & jferran6@xtec.cat';
-$modversion['admin'] = 1;
-$modversion['securityschema'] = array('IWmyrole::' => '::');
-$modversion['dependencies'] = array(array('modname' => 'IWmain',
-						'minversion' => '2.0',
-						'maxversion' => '',
-						'status' => ModUtil::DEPENDENCY_REQUIRED));
+
+class IWmyrole_Version extends Zikula_Version
+{
+    public function getMetaData() {
+        $meta = array();
+        $meta['displayname'] = $this->__("myRole");
+        $meta['description'] = $this->__("Allow users to change their roles or groups.");
+        $meta['url'] = $this->__("IWmyrole");
+        $meta['version'] = '3.0.0';
+        $meta['securityschema'] = array('IWmyrole::' => '::');
+        /*
+        $meta['dependencies'] = array(array('modname' => 'IWmain',
+                                            'minversion' => '3.0.0',
+                                            'maxversion' => '',
+                                            'status' => ModUtil::DEPENDENCY_REQUIRED));
+         *
+         */
+        return $meta;
+    }
+}
